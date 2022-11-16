334cd3498e8d24e0850f5b08845c679ac8d4ee89
KasunYogeeMedia <kasun.yogeemedia@gmail.com>
2022-11-16 19:48:31 +0530

    office

--- inc/script/script.js
+++ inc/script/script.js
@@ -1,10 +1,15 @@
-$(function () {
-    // Owl Carousel
-    var owl = $(".owl-carousel");
-    owl.owlCarousel({
-        items: 3,
-        margin: 10,
-        loop: true,
-        nav: true
-    });
+var owl = $('.owl-carousel');
+owl.owlCarousel({
+    items:3,
+    loop:true,
+    margin:10,
+    autoplay:true,
+    autoplayTimeout:2000,
+    autoplayHoverPause:true
 });
+$('.play').on('click',function(){
+    owl.trigger('play.owl.autoplay',[1000])
+})
+$('.stop').on('click',function(){
+    owl.trigger('stop.owl.autoplay')
+})
